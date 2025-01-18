21fb1f84c28c68a74871a2f54b21028f51537e41
Vasu Verma <vermavasu069@gmail.com>
2025-01-18 19:01:16 +0530

    Pre-final

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,6 +2,7 @@
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 
 export const Navigation: React.FC = () => {
 	const ref = useRef<HTMLElement>(null);
@@ -29,6 +30,12 @@ export const Navigation: React.FC = () => {
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
 						<Link
+						 href="/"
+						 className="duration-200 text-zinc-400 hover:text-zinc-100 font-bold"
+						>
+					     Vasu.dev 
+						</Link>
+						<Link
 							href="/projects"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
@@ -39,6 +46,12 @@ export const Navigation: React.FC = () => {
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							Contact
+						</Link>
+						<Link
+						href="/skills"
+						className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+                         Skills
 						</Link>
 					</div>
 
