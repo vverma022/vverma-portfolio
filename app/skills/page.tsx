263a29789b34c59c3aacbc4aa4588e634bef83fa
Vasu Verma <vermavasu069@gmail.com>
2025-01-18 17:30:38 +0530

    Skills page issue

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,40 +1,9 @@
 import React from "react";
-import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
-import { Redis } from "@upstash/redis";
 import { Skills } from "./skills";
 import { skills } from "content/skills/skills-list";
 
-const redis = Redis.fromEnv();
-
-export const revalidate = 60;
-export default async function ProjectsPage() {
-  const views = (
-    await redis.mget<number[]>(
-      ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
-    )
-  ).reduce((acc, v, i) => {
-    acc[allProjects[i].slug] = v ?? 0;
-    return acc;
-  }, {} as Record<string, number>);
-
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
-  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
-  const sorted = allProjects
-    .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.slug !== featured.slug &&
-        project.slug !== top2.slug &&
-        project.slug !== top3.slug,
-    )
-    .sort(
-      (a, b) =>
-        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
-    );
-
+export default function SkillsPage() {
   return (
     <div className="relative pb-16">
     <Navigation />
